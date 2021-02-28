a83475976d033a40056a53d463ae0e40b5d19228
Alicia Weenum <weenum.alicia@gmail.com>
2021-02-28 12:26:03 -0500

    Redid Budget Class

--- app/controllers/budgets_controller.rb
+++ app/controllers/budgets_controller.rb
@@ -4,4 +4,32 @@ class BudgetsController < ApplicationController
 
         render json: budgets.to_json(except: [:created_at, :updated_at])
     end
+
+    def create
+        budget = Budget.new(budget_params)
+        
+        if budget.save 
+            render json: budget.to_json(except: [:created_at, :updated_at])
+        else 
+            render json: {error: "No Transaction Entered."}
+        end
+    end
+
+    def show
+        budget = Budget.find(params[:id])
+        render json: budgets.to_json(except: [:created_at, :updated_at])
+    end
+
+    def destroy
+        budget = Budget.find(params[:id])
+        budget.destroy 
+        render json: {message: "successfully deleted #{budget.name}"}
+    end
+
+    private
+
+    def budget_params
+        params.require(:budget).permit(:name, :amount, :date)
+    end
+
 end
